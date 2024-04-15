744ed496c560dcd3908c6b5a3d5b522423556c2a
geonokown <furitstore1@naver.com>
2024-04-15 19:28:30 +0900

    :sparkles: 로그인 기능 추가
    로그인 기능 추가 및  jwt 토큰 형식 사용

--- frontend/index.js
+++ frontend/index.js
@@ -14,7 +14,7 @@ const calcTime = (timestamp) => {
 const renderData = (data) => {
     const main = document.querySelector("main");
 
-    data.reverse().forEach(async (obj) => {
+    data.sort((a, b) => b.insertAt - a.insertAt).forEach(async (obj) => {
         const div = document.createElement("div");
         div.className = "item-list";
 
@@ -53,7 +53,17 @@ const renderData = (data) => {
 };
 
 const fetchList = async () => {
-    const res = await fetch("/items");
+    const accessToken = window.localStorage.getItem("token");
+    const res = await fetch("/items", {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
+    if (res.status === 401) {
+        alert("로그인이 필요합니다.");
+        window.location.pathname = "/login.html";
+        return;
+    }
     const data = await res.json();
     renderData(data);
 };
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI,UploadFile,Form,Response
+from fastapi import FastAPI,UploadFile,Form,Response,Depends
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
+from fastapi_login import LoginManager
+from fastapi_login.exceptions import InvalidCredentialsException
 from typing import Annotated
 import sqlite3
 
@@ -23,39 +25,37 @@ cur.execute(f"""
 
 app = FastAPI()
 
-@app.post('/items')
-async def create_item(image:UploadFile,
-                title:Annotated[str,Form()], 
-                price:Annotated[int,Form()], 
-                description:Annotated[str,Form()], 
-                place:Annotated[str,Form()],
-                insertAt:Annotated[int,Form()]):
-    image_bytes = await image.read()
-    cur.execute(f"""
-                INSERT INTO items (title, image, price, description, place, insertAt)
-                VALUES ('{title}','{image_bytes.hex()}', {price}, '{description}', '{place}', {insertAt})
-                """)
-    con.commit()
-    return "200"
-    
-@app.get('/items')
-async def get_items():
-    # 컬럼명도 같이 가져옴.
+SERCTET = "kko"
+manager = LoginManager(SERCTET, '/login')
+
+@manager.user_loader()
+def query_user(data):
+    WHERE_STATEMENTS = f'id="{data}"'
+    if type (data) == dict:
+        WHERE_STATEMENTS = f'''id="{data['name']}"'''
     con.row_factory = sqlite3.Row
     cur = con.cursor()
-    rows = cur.execute(f"""
-                       SELECT * FROM items
-                        """).fetchall()
-    return JSONResponse(jsonable_encoder(dict(row) for row in rows))
+    user = cur.execute(f"""
+                        SELECT * FROM users WHERE {WHERE_STATEMENTS}
+                        """).fetchone()
+    return user
 
-@app.get('/images/{item_id}')
-async def get_image(item_id):
-    cur = con.cursor()
-    # 16진법
-    image_bytes = cur.execute(f"""
-                            SELECT image FROM items WHERE id ={item_id}
-                            """).fetchone()[0]
-    return Response(content=bytes.fromhex(image_bytes), media_type='image/*')
+@app.post('/login')
+def login(id : Annotated[str, Form()], 
+            password : Annotated[str, Form()]):
+    user = query_user(id)
+    if not user :
+        raise InvalidCredentialsException
+    elif password != user['password']:
+        raise InvalidCredentialsException
+    access_token = manager.create_access_token(data = {
+        'sub':{
+            'id': user['id'],
+            'name': user['name'],
+            'email': user['email']
+        }
+    })
+    return {'access_token' : access_token}
 
 
 @app.post('/signup')
@@ -74,10 +74,40 @@ def signup(id : Annotated[str, Form()],
 
 
 
+@app.post('/items')
+async def create_item(image:UploadFile,
+                title:Annotated[str,Form()], 
+                price:Annotated[int,Form()], 
+                description:Annotated[str,Form()], 
+                place:Annotated[str,Form()],
+                insertAt:Annotated[int,Form()]
+                ):
+    image_bytes = await image.read()
+    cur.execute(f"""
+                INSERT INTO items (title, image, price, description, place, insertAt)
+                VALUES ('{title}','{image_bytes.hex()}', {price}, '{description}', '{place}', {insertAt})
+                """)
+    con.commit()
+    return "200"
+    
+@app.get('/items')
+async def get_items(user=Depends(manager)):
+    # 컬럼명도 같이 가져옴.
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    rows = cur.execute(f"""
+                       SELECT * FROM items
+                        """).fetchall()
+    return JSONResponse(jsonable_encoder(dict(row) for row in rows))
 
-
-
-
+@app.get('/images/{item_id}')
+async def get_image(item_id):
+    cur = con.cursor()
+    # 16진법
+    image_bytes = cur.execute(f"""
+                            SELECT image FROM items WHERE id ={item_id}
+                            """).fetchone()[0]
+    return Response(content=bytes.fromhex(image_bytes), media_type='image/*')
 
 
 
